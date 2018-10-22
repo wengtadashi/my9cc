767a61b503ca8dbe55e156daec940021868439ce
wengtadashi <motto.honobono@gmail.com>
2018-10-22 14:29:02 +0000

    add + - operator

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 try() {
   expected="$1"
   input="$2"
@@ -9,7 +8,9 @@ try() {
   ./tmp
   actual="$?"
 
-  if [ "$actual" != "$expected" ]; then
+  if [ "$actual" == "$expected" ]; then
+    echo "$input => $actual"
+  else
     echo "$input expected, but got $actual"
     exit 1
   fi
@@ -17,4 +18,6 @@ try() {
 
 try 0 0
 try 42 42
+try 21 '5+20-4'
+
 echo OK
